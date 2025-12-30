60f8159ac490b7cbae52d0aca13c1a33a2b5b090
yash joshi <yjoshi5107@gmail.com>
2025-12-30 23:05:24 +0530

    Fix CORS for Vercel frontend

--- app/main.py
+++ app/main.py
@@ -9,8 +9,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "https://purplemerit-frontend.onrender.com",  # when deployed
+        "https://purplemerit-frontend.vercel.app",
+        "https://purplemerit-frontend-jhvsudojw-yashjoshis-projects.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
